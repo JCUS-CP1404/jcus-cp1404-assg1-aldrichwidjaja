4e963fc39d0a289468f5b2a43646c61a34e2b86e
Aldrich Widjaja <50089724+aldrichwidjaja@users.noreply.github.com>
2019-07-30 23:06:14 +0800

    Add files via upload

--- assignment1.py
+++ assignment1.py
@@ -31,7 +31,14 @@ Q - Quit""").upper()
         elif menuinput == "M".upper():
             visit()
             menuinput = input("""Menu:
- L - List Places
+L - List Places
+A - Add new place
+M - Mark a place as visited
+Q - Quit""").upper()
+        else:
+            print("Invalid Choice! Please choose menu")
+            menuinput = input("""Menu:
+L - List Places
 A - Add new place
 M - Mark a place as visited
 Q - Quit""").upper()
@@ -41,21 +48,52 @@ def open1(): #READ CSV FILES AND DISPLAY
     import operator
     with open('places.csv', 'r') as csvFile:
         reader = csv.reader(csvFile)
-        datasort = sorted(reader, key=lambda row:(row[3]))
+        datasort = sorted(reader, key=lambda row:(row[3],(int(row[2]))))
         count = 0
+        row_count = sum(1 for row in datasort)
+        row_count1 = 0
         for row in datasort:
+            if row[3] == 'n':
+                row_count1 = row_count1 + 1
             count= count + 1
             notvis = row[3].replace('n', '*').replace('v', ' ')
-            print(notvis, '{:>1}'.format(count),'{:>0}'.format('.'), '{:<20}'.format(row[0]), '{:<20}'.format(row[1]), "Priority",'{:<20}'.format(row[2]))
+            print(notvis, '{:>1}'.format(count),'{:>0}'.format('.'), '{:<10}'.format(row[0]), "in", '{:<20}'.format(row[1]), "Priority",'{:<10}'.format(row[2]))
+        if row_count1 == 0:
+            print(row_count, "Places, No places left to visit. Why not add a new place?")
+        else:
+            print(row_count, "Places, you still want to visit", row_count1, "places")
 
     csvFile.close()
 
 def add(): #WRITE CSV FILES IN NEW LINE
     import csv
-    x = str(input("Please Input city name"))
-    y = str(input("Please Input country name"))
-    z = int(input("Please Input priority"))
-    vn = "n"
+    while True:
+        x = input("Name: ")
+        if x.isalpha() or '':
+            break
+        print("Invalid! Input Can't be blank / input only in alphabets")
+    while True:
+        y = input("Country: ")
+        if y.isalpha() or '':
+            break
+        print("Invalid! Input Can't be blank / input only in alphabets")
+
+    class NotPositiveError(UserWarning):
+        pass
+
+    while True:
+        z = input("Priority: ")
+        try:
+            number = int(z)
+            if number <= 0:
+                raise NotPositiveError
+            break
+        except ValueError:
+            print("Invalid Input! Enter a valid number")
+        except NotPositiveError:
+            print("Number must be > 0")
+
+    vn = "n" #vn = mark non-visited
     print(x, "In", y, "Priority", z, "Has been added to travel tracker")
     newrow = [x, y, z, vn]
 
@@ -64,13 +102,28 @@ def add(): #WRITE CSV FILES IN NEW LINE
         writer.writerow(newrow)
     csvFile.close()
 
-def visit():
+def visit(): #Function to mark visited places
     import csv
-    x = int(input("Please choose place to visit"))
 
     with open('places.csv', 'r') as csvFile:
         reader = csv.reader(csvFile)
-        datasort = sorted(reader, key=lambda row: (row[3]))
+        datasort = sorted(reader, key=lambda row: (row[3], (int(row[2]))))
+
+
+    while True:
+        try:
+            x = int(input("Please choose place to visit"))
+            if x > sum(1 for row in datasort):
+                print("Invalid place number!")
+                continue
+            elif x <= 0:
+                print("Invalid! Number must be > 0")
+                continue
+        except ValueError:
+            print("Invalid! Enter a valid number")
+            continue
+        else:
+            break
 
     with open('places.csv', 'w', newline='') as csvFile1:
         writer = csv.writer(csvFile1)
@@ -78,15 +131,16 @@ def visit():
         for row in datasort:
             num = num+1
             if num == x:
+                if row[3] is "v":
+                    print("Place is already visited")
+                else:
                     row[3] = "v"
-                writer.writerow(row)
+                    print(row[0], "in",row[1], "is visited")
+
+            writer.writerow(row)
 
     csvFile.close()
     csvFile1.close()
-
-
-
-
 
 main()
 
